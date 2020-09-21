ea79bb66e8bc0ba9e491078974a54bf6b6b61d2e
Sere27 <vetbat11@gmail.com>
2020-09-21 13:56:00 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+//Elementleri seçme
+
+const githubForm = document.getElementById("github-form");
+const nameInput = document.getElementById("githubname");
+const clearLastUsers = document.getElementById("clear-last-users");
+const lastUsers = document.getElementById("last-users");
+
+const github = new Github();
+
+eventListeners();
+
+function eventListeners() {
+	githubForm.addEventListener("submit", getData);
+	clearLastUsers.addEventListener("click", clearAllSearched);
+
+	document.addEventListener("DOMContentLoaded", getAllSearched);
+}
+
+function getData(e) {
+	//trim ile gereksiz boşlukları temizliyoruz
+	let username = nameInput.value.trim;
+
+	if (username === "") {
+		alert("Geçerli bir kullanıcı adı giriniz!");
+	} else {
+		github
+			.getGithubData(username)
+			.then((response) => console.log(response.user))
+			.catch((err) => console.log("err :>> ", err));
+	}
+
+	e.preventDefault();
+}
+
+function clearAllSearched() {
+	//tüm arananları temizle
+}
+
+function getAllSearched() {
+	//arananları storage al ui ekle
+}
